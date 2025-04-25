53347798757deba0818fd670c3addaacb5ff6c1c
Shivam Ross <144504100+shivam-ross@users.noreply.github.com>
2025-04-25 23:18:00 +0530

    Update page.tsx

--- apps/web/app/submission/page.tsx
+++ apps/web/app/submission/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import { useEffect, useState } from "react";
 import { Topbar } from "../components/topbar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Submission() {
-  
+  const session = useSession();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const [submissions, setSubmissions] = useState<{
       id: string,
       status: "PENDING" | "ACCEPTED" | "WRONG_ANSWER" | "TIME_LIMIT_EXCEEDED" | "RUNTIME_ERROR" | "COMPILE_ERROR",
@@ -11,7 +15,14 @@ export default function Submission() {
       problemName: string,
 }[]>([]);
 
+useEffect(()=> {
+  if(session.status == "unauthenticated"){
+    router.push("/signin");
+  }
+},[session, router])
+
 useEffect(() => {
+  setLoading(true);
     const fetchSubmission = async () => {
       const res = await fetch("/api/submission", {
         method: "GET",
@@ -23,12 +34,15 @@ useEffect(() => {
         const data = await res.json();
         setSubmissions(data);
       } else {
-        alert("Failed to fetch submission");
+        console.log("Failed to fetch submission");
       }
+      setLoading(false);
     };
     fetchSubmission();
 
 },[]);
+
+
   
 return (
     <div className="bg-purple-200 min-h-screen">
@@ -40,8 +54,7 @@ return (
                     <h2 className="text-lg font-bold">Language</h2>
                     <h2 className="text-lg font-bold">Status</h2>
                   </div>
-            
-                  {submissions.map((submission) => (
+                  {(loading) ? <div className="flex flex-col justify-center items-center h-full font-mono text-xl text-gray-800">Loading...</div> : submissions.map((submission) => (
                                   <div key={submission.id}>
                                     <div className="grid grid-cols-3 w-full justify-items-center my-2 px-3 border-b py-2 mx-3">
                   <h2 className="text-md text-black justify-self-start">{submission.problemName}</h2>
